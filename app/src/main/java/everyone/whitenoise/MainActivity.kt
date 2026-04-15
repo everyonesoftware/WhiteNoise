d626360f618a002dce5050e327b2794c9270be6a
Dan Schulte <dan.schulte.qub@live.com>
2026-04-15 21:41:55 +0200

    Fix keystore decode file location so that the release build can find it

--- app/src/main/java/everyone/whitenoise/MainActivity.kt
+++ app/src/main/java/everyone/whitenoise/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-// Simple change to trigger GitHub action
-
 class MainActivity : AppCompatActivity() {
     private var isPlaying = false
 
